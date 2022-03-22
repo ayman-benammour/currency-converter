b5222fee30eb48c0b732a37cde185992385768ed
ayman-benammour <benammourayman@gmail.com>
2022-03-22 16:37:40 +0100

    :tada: Init project

---
+++ includes/config.php
@@ -0,0 +1,5 @@
+<?php
+
+// Show errors
+error_reporting(E_ALL);
+ini_set('display_errors', true);
---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+
+// Config
+include './includes/config.php'
+
+?>
+
+<!-- Header -->
+<?php include './chunks/header.php' ?>
+
+    <!-- Header -->
+    <header class="header">
+
+        <!-- Grid -->
+        <div class="grid">
+
+            <!-- Navigation -->
+            <nav class="navigation">
+                <a href="#" class="convertButton buttonActive">Convert</a>
+                <a href="#" class="currenciesButton">Currencies</a>
+                <a href="#" class="aboutButton">About</a>
+            </nav>
+
+            <div class="description">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A laborum porro accusantium incidunt est distinctio ut quasi rem at aspernatur quae culpa consectetur sed ipsam neque, qui nostrum sequi beatae?</p>
+            </div>
+
+            <div class="illustration">
+                <img src="./assets/images/illustration.svg" alt="Illustration">
+            </div>
+
+        </div>
+
+    </header>
+
+    <!-- Content -->
+    <main class="content">
+
+    </main>
+
+<?php include './chunks/footer.php' ?>
